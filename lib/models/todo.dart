059a2cc8f34bab2b229cb496017467f2eec34408
Nurizko Maulana <nurizko.maulana@gmail.com>
2021-06-03 23:43:05 +0700

    Add Todo Model

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -2,10 +2,10 @@
 //-----------------------------------------------------------------------------------------------------------------------------
 //? Things to do:
 // Define the following methods:
-//   a. all the getters and setters
-//   b. the 'copy' constructor
-//   c. the 'fromJson' constructor
-//   d. the 'toJson' method
+//   [DONE]a. all the getters and setters
+//   [DONE]b. the 'copy' constructor
+//   [DONE]c. the 'fromJson' constructor
+//   [DONE]d. the 'toJson' method
 //-----------------------------------------------------------------------------------------------------------------------------
 
 class Todo {
@@ -14,6 +14,21 @@ class Todo {
   bool _done;
   int _id;
   int _user;
+
+  get title => _title;
+  set title(value) => _title = value;
+
+  get description => _description;
+  set description(value) => _description = value;
+
+  get done => _done;
+  set done(value) => _done = value;
+
+  get id => _id;
+  set id(value) => _id = value;
+
+  get user => _user;
+  set user(value) => _user = value;
 
   Todo(
       {String title = '',
@@ -26,4 +41,28 @@ class Todo {
         _done = done,
         _id = id,
         _user = user;
+
+  Todo.copy(Todo from)
+      : this(
+            title: from._title,
+            description: from._description,
+            done: from._done,
+            id: from._id,
+            user: from._user);
+
+  Todo.fromJson(Map<String, dynamic> json)
+      : this(
+            title: json['title'] ?? '',
+            description: json['description'] ?? '',
+            done: json['done'] ?? false,
+            id: json['id'],
+            user: json['user']);
+
+  Map<String, dynamic> toJson() => {
+        'title': title,
+        'description': description,
+        'done': done,
+        'id': id,
+        'user': user
+      };
 }
